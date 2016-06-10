3351f779cfbfa91397ec003c80d5a08691a5e7fd
Adrian Juszczak <adrian@juszczak.io>
2016-06-10 19:23:46 +0200

    :wrench: gnu ls setup refactor :rainbow:
    Signed-off-by: Adrian Juszczak <adrian@juszczak.io>

--- bash/bash/utils/gls.sh
+++
@@ -1,16 +0,0 @@
-# gnu ls for osx
-# ~/.bash/utils/gls.sh
-# Adrian Juszczak
-
-# gnu environmen
-# PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
-# MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
-
-dircolors=~/.dircolors
-if [ -f $dircolors ]; then
-  eval `gdircolors -b $dircolors`
-fi
-
-alias ls="gls --color=always -F -I \"Icon\""
-alias grep="grep --color=always"
-alias egrep="egrep --color=always"
